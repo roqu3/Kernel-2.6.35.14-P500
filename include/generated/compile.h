215f8cb7a70bf87673b864c1c4e67c6c0e4384e4
RoQ <chroque@gmail.com>
2012-03-03 12:13:20 -0500

    update files

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 4 */
+/* This file is auto generated, version 7 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#4 PREEMPT Fri Feb 10 23:38:03 PET 2012"
-#define LINUX_COMPILE_TIME "23:38:03"
+#define UTS_VERSION "#7 PREEMPT Thu Mar 1 21:53:36 PET 2012"
+#define LINUX_COMPILE_TIME "21:53:36"
 #define LINUX_COMPILE_BY "roq"
 #define LINUX_COMPILE_HOST "roq"
 #define LINUX_COMPILE_DOMAIN "(none)"
