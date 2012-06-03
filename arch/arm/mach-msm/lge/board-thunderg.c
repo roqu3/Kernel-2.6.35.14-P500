8e4ce229656610dd665823feae7a33ada6865acd
RoQ <chroque@gmail.com>
2012-06-02 21:25:54 -0500

    test

--- arch/arm/mach-msm/lge/board-thunderg.c
+++ arch/arm/mach-msm/lge/board-thunderg.c
@@ -423,7 +423,7 @@ static struct msm_acpu_clock_platform_data msm7x2x_clock_data = {
 	.acpu_switch_time_us = 50,
 	.max_speed_delta_khz = 256000,
 	.vdd_switch_time_us = 62,
-	.max_axi_khz = 160000,
+	.max_axi_khz = 200000,
 };
 
 void msm_serial_debug_init(unsigned int base, int irq,
@@ -462,7 +462,7 @@ static void __init msm7x2x_init(void)
 #endif
 
 	if (cpu_is_msm7x27())
-		msm7x2x_clock_data.max_axi_khz = 200000;
+		msm7x2x_clock_data.max_axi_khz = 300000;
 
 	msm_acpu_clock_init(&msm7x2x_clock_data);
 
